25243007997f65fafd9c7812be240a02e34d7c10
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-30 03:26:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
 
-
+def my_collect(array)
+  i = 0
+  collection = []
+  if array.length > 0
+    while i < array.length
+      collection << yield(array[i])
+      i += 1
+    end
+  else
+    puts "passed array is empty"
+  end
+  collection
+end
